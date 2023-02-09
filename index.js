42acd0ac16a89213e76189b0a8e1e787f893e4fb
Valerii Ruchko <valerii.ruchko@gmail.com>
2023-02-09 18:38:18 +0100

    Completed backend part of the Apartment app

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
+const cors = require("cors");
 // establishing the connection
 const Apartment = require("./models/Apartment");
 const {
@@ -16,6 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // now we can serve static files to the client
 app.use(express.static("public"));
+app.use(cors());
 // syncing our model with our database and catching all errors
 Apartment.sync()
   .then((result) => {
